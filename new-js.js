87e4474a746f868f70d7852cb7ae3d254dde81bc
abhijithani <abhijithmscr7@gmail.com>
2025-06-10 18:15:31 +0530

    card addd to local storage

--- new-js.js
+++ new-js.js
@@ -1,19 +1,8 @@
 const taskContainer = document.querySelector(".task__container");
+const globalStore = [];//array of objects
 console.log(taskContainer);
 
-
-const saveChanges = () => {
-    const taskData = {
-        id: `${Date.now()}`,
-        imageUrl: document.getElementById("imageurl").value,
-        taskTitle: document.getElementById("tasktitle").value,
-        taskType: document.getElementById("tasktype").value,
-        taskDescription: document.getElementById("taskdescription").value,
-
-
-    }
-
-const newCard = `
+const generateNewCard = (taskData) => `  
 <div class="col-sm-12 col-md-6  col-lg-4 id=${taskData.id}">
         <div class="card">
           <div class="card-header d-flex  justify-content-end gap-2">
@@ -29,10 +18,51 @@ const newCard = `
         </div>
       </div>
 
-`;
+`
 
-taskContainer.insertAdjacentHTML("beforeend",newCard);
+
+const loadIntialCardData = () => {
+
+  //localstorage  to get tasky  card data
+  const getCardData = localStorage.getItem("tasky");
+
+  //convert to normal object 
+  const {cards} = JSON.parse(getCardData);
+
+  //loop over the array of object to create HTML card, inject it to DOM
+  cards.map((cardobject) => {
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardobject));
+
+    //update our globalstore
+    globalStore.push(cardobject);
+
+  });
+};
+
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+
+
+  };
+
+
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+  globalStore.push(taskData);
+  localStorage.setItem("tasky",JSON.stringify({cards:globalStore }));
+
 
 };
 
 //${xyz} dynamic value, can change in the near future
+
+//issues
+
+//page reference cause the data get deleted
+
+//features -delete edit open
